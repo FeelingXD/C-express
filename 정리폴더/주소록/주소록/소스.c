4d979e38be206beecaa52d67dd4f51463cdfb5d7
FeelingXD <wlals425315@gmail.com>
2020-04-28 19:11:33 +0900

    4-28/2

--- 정리폴더/주소록/주소록/소스.c
+++ 정리폴더/주소록/주소록/소스.c
@@ -15,34 +15,36 @@ void print_record(FILE *fp);
 void add_record(FILE *fp);
 void search_record(FILE *fp);
 void update_record(FILE *fp);
-
 int main() {
 
 	FILE *fp;
 	fp = fopen("address.dat" , "w+");
 	if (fp == NULL) {
-		printf("파일을 열수없습니다.");
+		printf("파일을 열수없습니다.\n");
 		exit(1);
 	}
 	fclose(fp);
+	fp = fopen("address.dat", "r+");
 	while (1)
 	{	
 		int c;
 		menu();
-		pritnf("사용할 옵션 :");
+		printf("사용할 옵션 :");
 		scanf("%d", &c);
+		
 		switch (c)
 		{
 		case 1:
-			
+			add_record(fp); break;
 		case 2:
-
+			update_record(fp); break;
 		case 3:
-
+			search_record(fp); break;
 		case 4:
+			printf("20174222 고지민\n");
+			system("pause");
+			fclose(fp);
 			return 0;
-
-			
 		}
 	}
 
@@ -50,9 +52,65 @@ int main() {
 void menu()
 {
 		printf("==============================\n");
-		printf("1");
-		printf("2");
-		printf("3");
-		printf("4");
+		printf("1. 추가\n");
+		printf("2. 수정\n");
+		printf("3. 검색\n");
+		printf("4. 종료\n");
 		printf("=============================\n");
+}
+PERSON get_record()
+{
+	
+	PERSON data;
+	getchar();
+	printf("이름 :");	gets(data.name);
+	printf("주소 :");	gets(data.adress);
+	printf("휴대폰 :");	gets(data.mobilephone);
+	printf("특징 :");	gets(data.desc);
+	
+	return data;
+}
+void add_record(FILE *fp)
+{
+	PERSON data;
+	data = get_record();
+	fseek(fp, 0, SEEK_END);
+	fwrite(&data, sizeof(data), 1, fp);
+	
+
+}
+void print_record(PERSON data)
+{
+	printf("이름 : %s\n", data.name); printf("주소 : %s\n", data.adress);
+	printf("휴대폰 : %s\n", data.mobilephone); printf("특징: %s\n", data.desc);
+}
+void search_record(FILE *fp)
+{
+	char name[SIZE];
+	PERSON data;
+
+	fseek(fp, 0, SEEK_SET);
+	getchar();
+	printf("검색할 사람이름 ");
+	gets(name);
+	while (!feof(fp))
+	{
+		fread(&data, sizeof(data), 1, fp);
+		if (strcmp(data.name, name) == 0)
+		{
+			print_record(data);
+			break;
+		}
+		else
+		{
+			printf("없음 \n");
+			break;
+		}
+	}
+
+}
+void update_record(FILE *fp)
+{
+
+
 }
