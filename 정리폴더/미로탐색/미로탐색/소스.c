bbc488fa9b058b6ded94f0cb63ce65d9f72a9929
FeelingXD <wlals425315@gmail.com>
2020-04-30 21:12:41 +0900

    4/30

---
+++ 정리폴더/미로탐색/미로탐색/소스.c
@@ -0,0 +1,74 @@
+#define MAX_STACK_SIZE 100
+#define MAZE_SIZE 6
+#include <stdio.h>
+//typedef int element;
+typedef struct {
+	short r; // row #
+	short c; // column#
+}element;
+
+typedef struct {
+	element stack[MAX_STACK_SIZE];
+	int top;
+}StackType;
+void init_stack(StackType *a)
+{
+	a->top = -1;
+}
+//공백 검출
+int is_empty(StackType *a)
+{
+	return(a->top == -1);
+}
+//포화 상태검출
+int is_full(StackType *a)
+{
+	return(a->top == (MAX_STACK_SIZE - 1));
+}
+void push(StackType *a, element item)
+{
+	if (is_full(a))
+	{
+		fprintf(stderr, "스택 포화 에러\n");
+		return;
+
+	}
+	else a->stack[++(a->top)] = item;
+}
+//삭제 함수
+void printStack(StackType *a) {
+
+	int i = a->top;
+	for (; 0 <= i; i--) {
+		printf("%d 번째 원소 %d\n", i, a->stack[i]);
+	}
+}
+element pop(StackType *a) {
+	if (is_empty(a))
+	{
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else
+	{
+		return a->stack[(a->top)--];
+	}
+}
+element peek(StackType *a) {
+
+	if (is_empty(a))
+	{
+		fprintf(stderr, "스택공백에러");
+		exit(1);
+	}
+	else return a->stack[a->top];
+}
+element here = { 1,0 }, entry = { 1,0 };
+char maze[MAZE_SIZE][MAZE_SIZE] = {
+	{'1','1','1,','1','1','1'},
+	{'e','0','1','0','0','1'},
+	{'1','0','0','0','1','1'},
+	{'1','0','1','0','1','1'},
+	{'1','0','1','0','0','1'},
+	{'1','1','1','1','1','1'},
+};
