adc07a831567feaf289026486f8e2c31b8e1716e
FeelingXD <wlals425315@gmail.com>
2020-06-01 17:28:24 +0900

    자료구조

--- 자료구조/단일리스트 6-1 구현/단일리스트 6-1 구현/소스.c
+++ 자료구조/단일리스트 6-1 구현/단일리스트 6-1 구현/소스.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 typedef int element;
-typedef struct ListNode {
+typedef struct {
 	element data;
 	struct ListNode* link;
 }ListNode;
---
+++ 자료구조/이진트리/이진트리/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+typedef struct TreeNode {
+	int data;
+	struct TreeNode *left, *right;
+}TreeNode;
+//			  15
+//		4			 20
+//	1			 16		 25
+TreeNode n1={ 1,NULL,NULL };
+TreeNode n2={ 4, &n1, NULL };
+TreeNode n3={ 16,NULL,NULL };
+TreeNode n4={ 25,NULL,NULL };
+TreeNode n5={ 20, &n3, &n4 };
+TreeNode n6={ 15, &n2, &n5 };
+TreeNode* root = &n6;
+//중위순회
+inorder(TreeNode* root) {
+	if (root)
+	{
+		inorder(root->left);
+		printf("%d\t", root->data);
+		inorder(root->right);
+	}
+}
+preorder(TreeNode* root) {
+	if (root)
+	{
+		printf("%d\t", root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+postorder(TreeNode* root)
+{
+	if (root)
+	{
+		postorder(root->left);
+		postorder(root->right);
+		printf("%d\t", root->data);
+	}
+}
+void main()
+{
+	inorder(root);
+	printf("\n");
+	preorder(root);
+	printf("\n");
+	postorder(root);
+}
